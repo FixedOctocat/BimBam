0935bb4c57748c0a60d8c23d828f46da88a7f700
FixedOctocat <egorfilatov2002@yandex.ru>
2023-10-31 13:52:37 +0300

    classes reworked

---
+++ classes/Analyzer.py
@@ -0,0 +1,122 @@
+import re
+import json
+from glob import glob
+
+
+class Analyzer:
+    def __init__(self, Apk, Settings):
+        self.Apk = Apk
+        self.Settings = Settings
+
+    def GetEntryPoints(
+        self, StartPoint=None, DepthNumber=0, AnalyzedClass=[], PreviousFunctionCalls=[]
+    ):
+        result = {
+            "Name": "",
+            "members": "",
+        }
+
+        if not self.Settings.Recursive:
+            if StartPoint in AnalyzedClass:
+                return []
+            AnalyzedClass.append(StartPoint)
+
+        if DepthNumber == self.Settings.Depth:
+            return []
+
+        if self.Settings.Details:
+            Detailed = list(
+                set(
+                    [
+                        FunctionCallFromFP
+                        for FunctionCallFromFP in PreviousFunctionCalls
+                        if StartPoint.split(".")[-1] in FunctionCallFromFP[0]
+                    ]
+                )
+            )
+
+            if not self.Settings.InitFunctions:
+                Detailed = [
+                    f"{FunctionCallFromFP[1]}({FunctionCallFromFP[2]}){FunctionCallFromFP[3]}"
+                    for FunctionCallFromFP in Detailed
+                    if FunctionCallFromFP[1] != "<init>"
+                ]
+
+            if Detailed:
+                for i in range(len(Detailed)):
+                    result[f"Function {i}"] = Detailed[i]
+
+        def ClassCheck(ClassName, FuncName, StartPoint=StartPoint):
+            if not self.Settings.PackageNameCheck:
+                for i in self.Apk.GetPackageName().split("."):
+                    if i not in ClassName:
+                        return False
+
+            if "android" in ClassName:
+                return False
+
+            if "java" in ClassName:
+                return False
+
+            if not self.Settings.InitFunctions:
+                if FuncName == "<init>":
+                    return False
+
+            if StartPoint.split(".")[-1] in ClassName:
+                return False
+
+            return True
+
+        ActivityFolder = [
+            dir
+            for dir in glob(
+                f"apktoolFolder/smali*/{'/'.join(StartPoint.split('.')[:-1])}"
+            )
+        ][0]
+
+        FirstPoint = open(
+            f"{ActivityFolder}/{StartPoint.split('.')[-1]}.smali", "r"
+        ).read()
+
+        FunctionCallsFromFP = re.findall(
+            "L([a-zA-Z0-9$\/<>]*);->([a-zA-Z0-9$\/<>]*)\(([a-zA-Z0-9$\/<>;\[\]]*)\)([a-zA-Z0-9$\/<>]*)",
+            FirstPoint,
+        )
+
+        AfterEntryPoints = list(
+            set(
+                [
+                    i[0].replace("/", ".")
+                    for i in FunctionCallsFromFP
+                    if ClassCheck(i[0], i[1])
+                ]
+            )
+        )
+
+        DetaildEP = []
+        if len(AfterEntryPoints):
+            for i in AfterEntryPoints:
+                EP = self.GetEntryPoints(
+                    i,
+                    DepthNumber=DepthNumber + 1,
+                    AnalyzedClass=AnalyzedClass,
+                    PreviousFunctionCalls=FunctionCallsFromFP,
+                )
+
+                if EP:
+                    DetaildEP.append(EP)
+
+        result["Name"] = f"{StartPoint.split('.')[-1]}"
+        result["members"] = DetaildEP
+
+        return result
+
+    def DrawClassGraph(self):
+        data = self.GetEntryPoints(
+            self.Settings.MainPoint
+            if self.Settings.MainPoint
+            else self.Apk.GetMainActivityName()
+        )
+
+        with open(f"{self.Settings.Output}.json", "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from classes.APK import Apk
 from classes.Settings import Settings
+from classes.Analyzer import Analyzer
 
 if __name__ == "__main__":
     ProgramSettings = Settings()
     ProgramSettings.InitArgparser()
-    apk = Apk(ProgramSettings.apkPath)
     ProgramSettings.PrintSettings()
 
+    apk = Apk(ProgramSettings.apkPath)
+
+    analyzer = Analyzer(apk, ProgramSettings)
+
     if ProgramSettings.Graph:
-        apk.DrawClassGraph(ProgramSettings)
+        analyzer.DrawClassGraph()
