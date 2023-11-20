df1f99f5d803a1b26c2e48619f2d654179eb232c
FixedOctocat <egorfilatov2002@yandex.ru>
2023-11-20 15:29:53 +0300

    Add if statement

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """Import classes"""
-from classes.apk import Apk, AndroidManifest
+from classes.apk import Apk
 from classes.settings import Settings
-from classes.analyzer import Analyzer
+from classes.analyzer import CallSearch
 from classes.graph import Graph
 
 if __name__ == "__main__":
@@ -12,9 +12,10 @@ if __name__ == "__main__":
 
     ApkFile = Apk(ProgramSettings.apk_path)
 
-    AnalyzerTool = Analyzer(ApkFile, ProgramSettings)
-    AnalyzerTool.start()
-    if ProgramSettings.pyvis:
-        MainGraph = Graph(f"{ProgramSettings.output}.json")
-        MainGraph.start()
-        MainGraph.show()
+    if ProgramSettings.functions_graph or ProgramSettings.intents_graph:
+        AnalyzerTool = CallSearch(ApkFile, ProgramSettings)
+        AnalyzerTool.start()
+        if ProgramSettings.pyvis:
+            MainGraph = Graph(f"{ProgramSettings.output}.json")
+            MainGraph.start()
+            MainGraph.show()
